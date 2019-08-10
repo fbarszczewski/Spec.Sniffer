cf7f0636bd318f1d62f9bd26c7ede144e927ac8e
fbars <fbarszczewski@outlook.com>
2019-08-10 23:05:49 +0200

    BAterry statu start

---
+++ Spec.Sniffer.Model/Battery/Battery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spec.Sniffer.Model.Battery
+{
+    public class Battery
+    {
+        public int Health { get; set; }
+        public int PowerLeft { get; set; }
+        public bool? IsCharging { get; set; }
+        public int ChargeRate { get; set; }
+        public string Serial { get; set; }
+
+    }
+}
---
+++ Spec.Sniffer.Model/Battery/BatteryStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spec.Sniffer.Model.Battery
+{
+    public class BatteryStatus
+    {
+        public List<Battery> Batteries { get; set; }
+    }
+}
