6dbb6939a1ad09813a9a62d54294e76a1abe3c12
fbars <fbarszczewski@outlook.com>
2019-08-11 19:13:50 +0200

    batt work2

--- Spec.Sniffer.Model/Battery/Battery.cs
+++ Spec.Sniffer.Model/Battery/Battery.cs
@@ -2,15 +2,33 @@
 {
     public class Battery
     {
+        /// <summary>
+        /// Battery name id.
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// Identifies the battery. Closest to serial
+        /// </summary>
         public string UniqueId { get; set; }
+        /// <summary>
+        /// Battery capacity remaining in %.
+        /// </summary>
         public int Health { get; set; }
+        /// <summary>
+        /// Current capacity remaining in %.
+        /// </summary>
         public int PowerLeft { get; set; }
+        /// <summary>
+        /// Is battery charging.
+        /// </summary>
         public bool IsCharging { get; set; }
+        /// <summary>
+        /// Charge or discharge rate. In mWh.
+        /// </summary>
         public int ChargeRate { get; set; }
 
-        public uint DesignedCapacity { get; set; }
-        public uint FullChargedCapacity { get; set; }
-        public uint EstimatedChargeRemaining { get; set; }
+        public uint DesignedCapacity { private get; set; }
+        public uint FullChargedCapacity { private get; set; }
+        public uint EstimatedChargeRemaining { private get; set; }
     }
 }
