e2df47e1c90402dac2b1358d47679ce512e80f9c
fbars <fbarszczewski@outlook.com>
2019-08-12 19:46:13 +0200

    Battery GUI 1

--- Spec.Sniffer/Properties/Resources.Designer.cs
+++ Spec.Sniffer/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace Spec.Sniffer.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap battery {
+            get {
+                object obj = ResourceManager.GetObject("battery", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap MicPlay {
             get {
                 object obj = ResourceManager.GetObject("MicPlay", resourceCulture);
