49fdcb14eac4742bde94d9e88021d89fcd05c26c
fbars <fbarszczewski@outlook.com>
2019-08-13 21:18:30 +0200

    Diagnostic GUI

--- Spec.Sniffer/Properties/Resources.Designer.cs
+++ Spec.Sniffer/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace Spec.Sniffer.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap DriversError {
+            get {
+                object obj = ResourceManager.GetObject("DriversError", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Ethernet {
             get {
                 object obj = ResourceManager.GetObject("Ethernet", resourceCulture);
